b837611f629853e693d01489a090a2f32cb1d2cc
Will Jukes <william.r.jukes@gmail.com>
2020-11-28 12:40:52 -0500

    removing old junk

--- chrtypes.h
+++
@@ -1,9 +0,0 @@
-#ifndef chrtypes_h
-/* begin chrtypes.h */
-#define chrtypes_h
-
-#include "obj.h"
-
-
-/* end chrtypes.h */
-#endif
--- lambda.h
+++
@@ -1,7 +0,0 @@
-#ifndef lambda_h
-/* begin lambda.h */
-
-#define lambda_h
-
-/* end lambda.h */
-#endif
